0537561620431e40941c44ef0dedc0456b47ea8b
robbieardison <ardisonouus@hotmail.com>
2022-08-27 12:00:13 +0700

    "Another commitment"

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,18 +1,22 @@
+import { render } from '@testing-library/react';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>Ja<span className="highlight">mmm</span>ing</h1>
-      <div className="App">
-    //Add a SearchBar component//
-      <div class="App-playlist">
-      //Add a SearchResults component//
-      //dd a Playlist component//
-    </div>
-  </div>
-</div>
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          {/* <SearchBar /> */}
+        <div class="App-playlist">
+          {/* <SearchResult /> */}
+          {/* <Playlist /> */}
+        </div>
+        </div>
+      </div>
   );
+}
 }
 
 export default App;
