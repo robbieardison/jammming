10b9b9e018d75f38aada628b2f544f11e096ebba
roger cassano <robbieardison@Robbies-MacBook-Pro.local>
2022-08-27 11:18:57 +0700

    all changes in Step 1

---
+++ src/Components/App/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Ja<span className="highlight">mmm</span>ing</h1>
+      <div className="App">
+    //Add a SearchBar component//
+      <div class="App-playlist">
+      //Add a SearchResults component//
+      //dd a Playlist component//
+    </div>
+  </div>
+</div>
+  );
+}
+
+export default App;
