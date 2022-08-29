b6d031ec1ff0a75d5a11f9f301af5dc798fcb69d
robbieardison <ardisonouus@hotmail.com>
2022-08-29 21:38:41 +0700

    savePlaylist added as onSave to Playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -25,6 +25,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
   addTrack(track) {
     let tracks = this.state.playlistTracks;
@@ -45,6 +46,10 @@ class App extends React.Component {
   updatePlaylistName(name) {
     this.setState({ playlistName: name})
   }
+
+  savePlaylist() {
+    const trackUris = this.state.playlistTracks.map(track => track.id);
+  }
    
 
   render() {
@@ -58,7 +63,8 @@ class App extends React.Component {
             <Playlist playlistName={this.state.playlistName}
                       playlistTracks={this.state.playlistTracks}
                       onRemove={this.removeTrack} 
-                      onNameChange={this.updatePlaylistName}/> 
+                      onNameChange={this.updatePlaylistName}
+                      onSave={this.savePlaylist}/> 
           </div>
         </div>
       </div>
