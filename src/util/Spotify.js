166607eed3c97c920fc5558e9f08026aceb59765
robbieardison <ardisonouus@hotmail.com>
2022-09-03 00:21:22 +0700

    added clientId api-key

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,8 +1,21 @@
+const clientId = 'enter your api-key here';
 let accessToken;
 
 const Spotify = {
     getAccessToken() {
         if (accessToken) {
+            return accessToken
+        }
+        //check for access token match 
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            //This clear  the parameters from the URL, so the app doesn’t try grabbing the access token after it has expired
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
             return accessToken
         }
     }
